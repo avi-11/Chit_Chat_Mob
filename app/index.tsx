cf199b7f8907e3f4e2bce5ba0083b5d9b91e4f56
avi-11 <singh.avi1109@gmail.com>
2024-02-25 16:05:19 +0530

    -- otp verification added --

--- app/index.tsx
+++ app/index.tsx
@@ -1,73 +1,76 @@
-import { View,Text,StyleSheet,Image,TouchableOpacity, Linking } from "react-native";
-import welcomeImg from "@/assets/images/welcome.png";
-import Colors from "@/constants/Colors";
-import { Link } from "expo-router";
-const welcome_Image=Image.resolveAssetSource(welcomeImg).uri;
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import Colors from '@/constants/Colors';
+import { Link } from 'expo-router';
+import welcomeImage from '@/assets/images/welcome.png';
+const welcome_image = Image.resolveAssetSource(welcomeImage).uri;
 
-const Page=()=>{
+const WelcomeScreen = () => {
+  const openLink = () => {
+    Linking.openURL('https://galaxies.dev');
+  };
 
-    const openLink=()=>{
-        Linking.openURL('https://github.com/avi-11');
-    }
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: welcome_image }} style={styles.welcome} />
+      <Text style={styles.headline}>Welcome to WhatsApp Clone</Text>
+      <Text style={styles.description}>
+        Read our{' '}
+        <Text style={styles.link} onPress={openLink}>
+          Privacy Policy
+        </Text>
+        . {'Tap "Agree & Continue" to accept the '}
+        <Text style={styles.link} onPress={openLink}>
+          Terms of Service
+        </Text>
+        .
+      </Text>
+      <Link href={'/otp'} replace asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Agree & Continue</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
+};
 
-    return(
-        <View style={styles.container}>
-            <Image source={{uri:welcome_Image}} style={styles.welcome}/>
-            <Text style={styles.headline}>Welcome to Chit-Chat</Text>
-            <Text style={styles.description}>
-                Read Our{" "}
-                <Text style={styles.link} onPress={openLink}>
-                    Privacy Policy
-                </Text>
-                . {'Tap "Agree & Continue" to accept the '}
-                <Text style={styles.link} onPress={openLink}>Terms of Service</Text>
-                .
-            </Text>
-            <Link href={"/otp"} asChild replace>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttontext}>Agree & Continue</Text>
-                </TouchableOpacity>
-            </Link>
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  welcome: {
+    width: '100%',
+    height: 300,
+    borderRadius: 60,
+    marginBottom: 80,
+  },
+  headline: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 80,
+    color: Colors.gray,
+  },
+  link: {
+    color: Colors.primary,
+  },
+  button: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: Colors.primary,
+    fontSize: 22,
+    fontWeight: '500',
+  },
+});
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        padding:20,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#fff'
-    },
-    welcome:{
-        width:'100%',
-        height:300,
-        marginBottom:80
-    },
-    headline:{
-        fontSize:24,
-        fontWeight:'bold',
-        marginVertical:20
-    },
-    description:{
-        fontSize:14,
-        textAlign:'center',
-        marginBottom:80,
-        color:Colors.gray
-    },
-    link:{
-        color:Colors.primary
-    },
-    button:{
-        width:'100%',
-        alignItems:'center'
-    },
-    buttontext:{
-        fontSize:22,
-        color:Colors.primary,
-        fontWeight:'bold'
-    }
-})
-
-export default Page;
+export default WelcomeScreen;
--- app/otp.tsx
+++ app/otp.tsx
@@ -1,163 +1,238 @@
-import { ActivityIndicator, Keyboard, KeyboardAvoidingView, Linking, Platform, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
-import React, { useState } from 'react'
-import { useRouter } from 'expo-router';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Linking,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
 import MaskInput from 'react-native-mask-input';
+import { isClerkAPIResponseError, useSignIn, useSignUp } from '@clerk/clerk-expo';
 
-const Otp = () => {
+const GER_PHONE = [
+  `+`,
+  /\d/,
+  /\d/,
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  
+];
 
-    const [loading,setLoading]=useState(false);
-    const [phoneNumber,setPhoneNumber]=useState('');
-    const router=useRouter();
-    const keyboardVerticalOffset=Platform.OS==='ios'?90:0;
-    const {bottom}=useSafeAreaInsets();
+const Page = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { signUp, setActive } = useSignUp();
+  const { signIn } = useSignIn();
 
-    const openLink=()=>{
-        Linking.openURL('https://github.com/avi-11');
+  const openLink = () => {
+    Linking.openURL('https://galaxies.dev');
+  };
+
+  const sendOTP = async () => {
+    console.log('sendOTP', phoneNumber);
+    setLoading(true);
+
+    try {
+      await signUp!.create({
+        phoneNumber,
+      });
+      console.log('TESafter createT: ', signUp!.createdSessionId);
+
+      signUp!.preparePhoneNumberVerification();
+
+      console.log('after prepare: ');
+      router.push(`/verify/${phoneNumber}`);
+    } catch (err) {
+      console.log('error', JSON.stringify(err, null, 2));
+
+      if (isClerkAPIResponseError(err)) {
+        if (err.errors[0].code === 'form_identifier_exists') {
+          // User signed up before
+          console.log('User signed up before');
+          await trySignIn();
+        } else {
+          setLoading(false);
+          Alert.alert('Error', err.errors[0].message);
+        }
+      }
     }
+  };
 
-    const sendOTP=async ()=>{
-        setLoading(true);
-        setTimeout(()=>{
-            setLoading(false)
-        },2000)
-    };
+  const trySignIn = async () => {
+    console.log('trySignIn', phoneNumber);
 
-    const trySignIn=async ()=>{
-        
-    }
+    const { supportedFirstFactors } = await signIn!.create({
+      identifier: phoneNumber,
+    });
+
+    const firstPhoneFactor: any = supportedFirstFactors.find((factor: any) => {
+      return factor.strategy === 'phone_code';
+    });
+
+    const { phoneNumberId } = firstPhoneFactor;
+
+    await signIn!.prepareFirstFactor({
+      strategy: 'phone_code',
+      phoneNumberId,
+    });
+
+    router.push(`/verify/${phoneNumber}?signin=true`);
+    setLoading(false);
+  };
 
   return (
-    <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.container}>
-            {loading && (
-            <View style={[StyleSheet.absoluteFill,styles.loading]}>
-                <ActivityIndicator size='large' color={Colors.primary}/>
-                <Text style={{fontSize:18,padding:10}}>Sending Code ...</Text>
-            </View>
-            
-            )}
-            <Text style={styles.description}>
-                Chit-Chat will need to verify your account.Carrier charges may apply.
-            </Text>
-            <View style={styles.list}>
-                <View style={styles.listitem}>
-                    <Text style={styles.listItemText}>India</Text>
-                    <Ionicons name="chevron-forward" size={20} color={Colors.gray}/>
-                </View>
-                <View style={styles.separator}/>
-                <MaskInput style={styles.input}
-                keyboardType='numeric'
-                autoFocus
-      value={phoneNumber}
-      onChangeText={(masked, unmasked) => {
-        setPhoneNumber(masked); // you can use the unmasked value as well
-
-        // assuming you typed "9" all the way:
-        // console.log(masked); // (99) 99999-9999
-        // console.log(unmasked); // 99999999999
-      }}
-      mask={['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/,/\d/]}
-    />
-            </View>
-            <Text style={styles.description}>
-                Read Our{" "}
-                <Text style={styles.link} onPress={openLink}>
-                    Privacy Policy
-                </Text>
-                . {'Tap "Agree & Continue" to accept the '}
-                <Text style={styles.link} onPress={openLink}>Terms of Service</Text>
-                .
-            </Text>
-            <View style={{flex:1}}/>
-            <TouchableOpacity disabled={phoneNumber===''} style={[styles.button,phoneNumber!==""?styles.enabled:null,{marginBottom:bottom}]} onPress={sendOTP}>
-                <Text style={[styles.buttontext,phoneNumber!==""?styles.enabled:null]}>Next</Text>
-            </TouchableOpacity>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={keyboardVerticalOffset}
+      style={{ flex: 1 }}
+      behavior="padding">
+      {loading && (
+        <View style={[StyleSheet.absoluteFill, styles.loading]}>
+          <ActivityIndicator size="large" color={Colors.primary} />
+          <Text style={{ fontSize: 18, padding: 10 }}>Sending code...</Text>
         </View>
-        </TouchableWithoutFeedback>
+      )}
+
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          WhatsApp will need to verify your account. Carrier charges may apply.
+        </Text>
+
+        <View style={styles.list}>
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>Germany</Text>
+            <Ionicons name="chevron-forward" size={20} color={Colors.gray} />
+          </View>
+          <View style={styles.separator} />
+
+          <MaskInput
+            value={phoneNumber}
+            keyboardType="numeric"
+            autoFocus
+            placeholder="+12 your phone number"
+            onChangeText={(masked, unmasked) => {
+              setPhoneNumber(masked);
+            }}
+            mask={GER_PHONE}
+            style={styles.input}
+          />
+        </View>
+
+        <Text style={styles.legal}>
+          You must be{' '}
+          <Text style={styles.link} onPress={openLink}>
+            at least 16 years old
+          </Text>{' '}
+          to register. Learn how WhatsApp works with the{' '}
+          <Text style={styles.link} onPress={openLink}>
+            Meta Companies
+          </Text>
+          .
+        </Text>
+
+        <View style={{ flex: 1 }} />
+
+        <TouchableOpacity
+          style={[styles.button, phoneNumber !== '' ? styles.enabled : null, { marginBottom: 20 }]}
+          onPress={sendOTP}>
+          <Text style={[styles.buttonText, phoneNumber !== '' ? styles.enabled : null]}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        padding:20,
-        backgroundColor:Colors.background,
-        gap:20
-    },
-    description:{
-        fontSize:14,
-        textAlign:'center',
-        
-        color:Colors.gray
-    },
-    list:{
-        backgroundColor:'#fff',
-        width:'100%',
-        borderRadius:10,
-        padding:10
-    },
-    listitem:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        padding:6,
-        marginBottom:10
-    },
-    listItemText:{
-        fontSize:18,
-        color:Colors.primary
-    },
-    separator:{
-        width:'100%',
-        height:StyleSheet.hairlineWidth,
-        backgroundColor:Colors.gray,
-        opacity:0.3
-    },
-    legal:{
-        fontSize:12,
-        textAlign:'center',
-        color:'#000'
-    },
-    link:{
-        color:Colors.primary
-    },
-    button:{
-        width:'100%',
-        alignItems:'center',
-        backgroundColor:Colors.lightGray,
-        padding:10,
-        borderRadius:10
-    },
-    enabled:{
-        backgroundColor:Colors.primary,
-        color:'#fff'
-    },
-    buttontext:{
-        color:Colors.gray,
-        fontSize:22,
-        fontWeight:'500'
-    },
-    input:{
-        backgroundColor:'#fff',
-        width:'100%',
-        padding:6,
-        fontSize:16,
-        marginTop:10
-    },
-    loading:{
-        ...StyleSheet.absoluteFillObject,
-        zIndex:10,
-        backgroundColor:'#fff',
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: Colors.background,
+    gap: 20,
+  },
+  description: {
+    fontSize: 14,
+    color: Colors.gray,
+  },
+  legal: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: '#000',
+  },
+  link: {
+    color: Colors.primary,
+  },
+  button: {
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: Colors.lightGray,
+    padding: 10,
+    borderRadius: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+    color: '#fff',
+  },
+  buttonText: {
+    color: Colors.gray,
+    fontSize: 22,
+    fontWeight: '500',
+  },
+  list: {
+    backgroundColor: '#fff',
+    width: '100%',
+    borderRadius: 10,
+    padding: 10,
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 6,
+    marginBottom: 10,
+  },
+  listItemText: {
+    fontSize: 18,
+    color: Colors.primary,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: Colors.gray,
+    opacity: 0.2,
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: '100%',
+    fontSize: 16,
+    padding: 6,
+    marginTop: 10,
+  },
 
-export default Otp;
+  loading: {
+    zIndex: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
+export default Page;
